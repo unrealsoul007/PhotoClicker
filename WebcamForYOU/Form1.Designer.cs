7af7e0944218d0ca4a7a7de32cc2c4fc97133406
Deepak Malik <deepak1993malik@gmail.com>
2014-12-31 16:45:30 +0530

    Added files for PhotoClicker
    All files for photoclicker added to the repository

---
+++ WebcamForYOU/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WebcamForYOU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mypic = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clickButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.MainSaveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.mirrorCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mypic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mypic
+            // 
+            this.mypic.Location = new System.Drawing.Point(2, 3);
+            this.mypic.Name = "mypic";
+            this.mypic.Size = new System.Drawing.Size(536, 369);
+            this.mypic.TabIndex = 0;
+            this.mypic.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(114, 403);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 406);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "List of Cameras:";
+            // 
+            // clickButton
+            // 
+            this.clickButton.Location = new System.Drawing.Point(307, 401);
+            this.clickButton.Name = "clickButton";
+            this.clickButton.Size = new System.Drawing.Size(75, 23);
+            this.clickButton.TabIndex = 3;
+            this.clickButton.Text = "Click..!!";
+            this.clickButton.UseVisualStyleBackColor = true;
+            this.clickButton.Click += new System.EventHandler(this.clickButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(438, 401);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "EXIT..";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // mirrorCheckBox
+            // 
+            this.mirrorCheckBox.AutoSize = true;
+            this.mirrorCheckBox.Location = new System.Drawing.Point(328, 441);
+            this.mirrorCheckBox.Name = "mirrorCheckBox";
+            this.mirrorCheckBox.Size = new System.Drawing.Size(70, 17);
+            this.mirrorCheckBox.TabIndex = 5;
+            this.mirrorCheckBox.Text = "MIRROR";
+            this.mirrorCheckBox.UseVisualStyleBackColor = true;
+            this.mirrorCheckBox.CheckedChanged += new System.EventHandler(this.mirrorCheckBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 515);
+            this.Controls.Add(this.mirrorCheckBox);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.clickButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.mypic);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WebcamForYOU-----Deepak Malik";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mypic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mypic;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button clickButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.SaveFileDialog MainSaveDialog;
+        private System.Windows.Forms.CheckBox mirrorCheckBox;
+    }
+}
+
---
+++ WebcamForYOU/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge.Video;
+using System.Threading;
+
+namespace WebcamForYOU
+{
+    public partial class Form1 : Form
+    {
+        public FilterInfoCollection CamsCollection;
+        public VideoCaptureDevice Cam = null;
+        int cameraSelected = 0;
+        int selectedIndex = 0;
+        int mirror = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CamsCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            comboBox1.Items.Clear();
+            mypic.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            foreach (FilterInfo fi in CamsCollection)
+            {
+                comboBox1.Items.Add(fi.Name);
+            }
+
+            this.KeyDown += Form1_KeyDown;
+            this.MaximizeBox = false;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            
+        }
+
+        /// <summary>
+        /// This method is called each time a new value is selected from the 
+        /// combobox. It selects the new camera selected by the user.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedIndex = comboBox1.SelectedIndex;
+            CameraStart();
+        }
+
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ImgSave();
+            }
+        }
+
+        #region Camera Start code
+        /// <summary>
+        /// This method Starts the camera.
+        /// </summary>
+        void CameraStart()
+        {
+            try
+            {
+                Cam = new VideoCaptureDevice(CamsCollection[selectedIndex].MonikerString);
+                Cam.NewFrame += Cam_NewFrame;
+                Cam.Start();
+                cameraSelected = 1;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No video input device found\n\nThe application will now EXIT... :(");
+                Thread.Sleep(200);
+                Application.Exit();
+            }
+        }
+        #endregion
+
+        void Cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap img = (Bitmap)eventArgs.Frame.Clone();
+            if (mirror == 0)
+            {
+                mypic.Image = img;
+            }
+            else
+            {
+                img.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                mypic.Image = img;
+            }
+        }
+
+        private void ImgSave()
+        {
+            try
+            {
+                MainSaveDialog.Filter = "(*.jpg)|*.jpg|AllFiles(*.*)|*.*";
+                if (MainSaveDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    mypic.Image.Save(MainSaveDialog.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please select a valid camera first..!!");
+            }
+        }
+
+        private void clickButton_Click(object sender, EventArgs e)
+        {
+            if (cameraSelected == 1)
+            {
+                ImgSave();
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Cam.SignalToStop();
+                Cam.WaitForStop();
+                Cam.Stop();
+            }
+            catch(NullReferenceException ex)
+            {
+                
+            }
+            finally
+            {
+                MessageBox.Show("SEE YOU NEXT TIME..!! :)");
+                Application.Exit();
+            }  
+        }
+
+        private void mirrorCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (mirrorCheckBox.Checked == true)
+            {
+                mirror = 1;
+            }
+            else
+            {
+                mirror = 0;
+            }
+        }
+    }
+}
